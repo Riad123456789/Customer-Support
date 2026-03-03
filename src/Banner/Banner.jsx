5f999c353a550324c4b783f2cab3a450edf732d7
Riad123456789 <riad80717@gmail.com>
2026-03-04 00:35:56 +0600

    foother create done

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='bg-gray-100'>
-      <div className='max-w-7xl mx-auto py-10'>
-        <div className='flex items-center justify-between gap-5 '>
-          <div className=' flex-1 bg-amber-200 h-48 rounded-md flex items-center justify-center'>
-            <div>
-              <p>In-Progress</p>
-              <p>0</p>
-            </div>
+    <div className='bg-gray-200'>
+      <div className='max-w-7xl mx-auto py-16'>
+        <div className='flex flex-col md:flex-row items-center justify-between gap-5'>
+          <div className='flex-1 bg bg-[#422AD5] h-48 rounded-md flex items-center justify-center text-white shadow-lg'>
+            <img className='' src="./public/vector1.png" alt="" />
+            <img src="./public/vector1.png" alt="" />
+           {/* <div className='text-center'>
+              <p className='text-lg font-medium'>Resolved</p>
+              <p className='text-4xl font-bold mt-2'>0</p>
+            </div> */}
           </div>
-          <div className='flex-1 bg-amber-200 h-48 rounded-md flex items-center justify-center'>
-            <div>
-              <p>Resolved</p>
-              <p>0</p>
-            </div>
+
+          <div className='flex-1 bg-gradient-to-r from-[#54CF68] to-[#00827A] h-48 rounded-md flex items-center justify-center text-white shadow-lg  '>
+                <img className='' src="./public/vector1.png" alt="" />
+            <img src="./public/vector1.png" alt="" />
+            {/* <div className='text-center'>
+              <p className='text-lg font-medium'>Resolved</p>
+              <p className='text-4xl font-bold mt-2'>0</p>
+            </div> */}
           </div>
         </div>
       </div>
