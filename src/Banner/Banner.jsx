414820635adb425e87648196af9fa44f97d6b2a0
Riad123456789 <riad80717@gmail.com>
2026-03-04 11:15:02 +0600

    Banner created done

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -4,23 +4,44 @@ const Banner = () => {
   return (
     <div className='bg-gray-200'>
       <div className='max-w-7xl mx-auto py-16'>
-        <div className='flex flex-col md:flex-row items-center justify-between gap-5'>
-          <div className='flex-1 bg bg-[#422AD5] h-48 rounded-md flex items-center justify-center text-white shadow-lg'>
-            <img className='' src="./public/vector1.png" alt="" />
-            <img src="./public/vector1.png" alt="" />
-           {/* <div className='text-center'>
-              <p className='text-lg font-medium'>Resolved</p>
-              <p className='text-4xl font-bold mt-2'>0</p>
-            </div> */}
+        <div className='flex flex-col md:flex-row items-center justify-between gap-8'>
+          <div className='flex-1  bg-linear-to-r from-[#632EE3] to-[#9F62F2] h-52 rounded-md  text-white '>
+            <div className='h-full flex justify-between items-center'>
+              <img
+                className='h-full object-contain '
+                src='./public/vector1.png'
+                alt=''
+              />
+
+              <div className='text-center'>
+                <p className='text-lg font-medium'>In-Progress</p>
+                <p className='text-5xl font-semibold mt-2'>0</p>
+              </div>
+              <img
+                className='h-full object-contain scale-x-[-1]'
+                src='./public/vector1.png'
+                alt=''
+              />
+            </div>
           </div>
 
-          <div className='flex-1 bg-gradient-to-r from-[#54CF68] to-[#00827A] h-48 rounded-md flex items-center justify-center text-white shadow-lg  '>
-                <img className='' src="./public/vector1.png" alt="" />
-            <img src="./public/vector1.png" alt="" />
-            {/* <div className='text-center'>
-              <p className='text-lg font-medium'>Resolved</p>
-              <p className='text-4xl font-bold mt-2'>0</p>
-            </div> */}
+          <div className='flex-1  bg-linear-to-r from-[#54CF68] to-[#00827A] h-52 rounded-md text-white '>
+            <div className='h-full flex justify-between items-center'>
+              <img
+                className='h-full object-contain '
+                src='./public/vector1.png'
+                alt=''
+              />
+              <div className='text-center'>
+                <p className='text-lg font-medium'>Resolved</p>
+                <p className='text-5xl font-semibold mt-2'>0</p>
+              </div>
+              <img
+                className='h-full object-contain scale-x-[-1]'
+                src='./public/vector1.png'
+                alt=''
+              />
+            </div>
           </div>
         </div>
       </div>
